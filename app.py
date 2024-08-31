7e21d5092caf1af05a0ae1cdcc75d43b9aec285e
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-09-01 01:36:52 +0500

    fix utc for python 3.10 deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, UTC
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ class MyTask(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(100), nullable=False)
     complete = db.Column(db.Integer, default=0)
-    created = db.Column(db.DateTime, default=datetime.now(UTC))
+    created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"Task {self.id}"
